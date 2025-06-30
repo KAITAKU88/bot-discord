b003c4a480e1c2067aa48467357c1e55a9b9bbb1
khaikaito <210374351+KAITAKU88@users.noreply.github.com>
2025-06-30 16:06:28 +0700

    Add repo bot discord

---
+++ index.js
@@ -0,0 +1,53 @@
+// index.js
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`✅ Bot đã sẵn sàng! (${client.user.tag})`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === '!ping') {
+    message.reply('🏓 Pong!');
+  }
+});
+
+//Gửi tin nhắn chào mừng khi có thành viên mới gia nhập: 
+client.on('guildMemberAdd', (member) => {
+//   Tìm kênh có tên chính xác
+  const channel = member.guild.channels.cache.find(
+    ch =>
+      ch.name.includes('chào-mừng-tự-giới-thiệu') &&
+      ch.type === 0 // type 0 là TextChannel (Discord.js v14)
+  );
+
+
+
+  if (channel) {
+    channel.send(`👋 Chào mừng <@${member.id}> đến với server! Hãy giới thiệu một chút về bạn nhé: `);
+  } else {
+    console.log('⚠️ Không tìm thấy kênh chào mừng!');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.DISCORD_TOKEN);
